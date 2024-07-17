d14265e237aa06e87c3096bf49ac1ffebdac922a
taseen06 <toufiqur.rahman.tasin@gmail.com>
2024-07-17 18:37:01 +0600

    third time

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const username="";
 const welcome= document.getElementById("welcome");
 welcome.textContent += (username=== "")? ` guest`: " bro";
 
+
+
 //username===""? welcome.textContent +=  ` guest`:welcome.textContent += ` bro`;
 console.log(document.title);
